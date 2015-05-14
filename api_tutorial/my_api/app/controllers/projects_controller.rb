e1bb55dc8a144654409805e9397f64cf52b6ddf7
Fred Chapman <areinei123@gmail.com>
2015-05-14 14:32:49 -0400

    Tutorials
    random tutorials i’ve completed

---
+++ api_tutorial/my_api/app/controllers/projects_controller.rb
@@ -0,0 +1,5 @@
+class ProjectsController < ApplicationController
+  def index
+    render json: {message: 'Resource not found'}
+  end 
+end
---
+++ api_tutorial/my_api/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+MyApi::Application.config.secret_token = 'fbd7c4a533fe4bb3559dfbf364373006d218900b0e4022c9256682e6ef6dc09817f18b9d798aef3eed7b605d7fdc56be0a550e3e35a53a6c64388f4773219aa6'
