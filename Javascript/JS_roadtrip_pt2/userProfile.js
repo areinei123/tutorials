e42ab44dcd0fba79c94f8d835f5e7b42d5e94794
Fred Chapman <areinei123@gmail.com>
2015-05-14 18:57:21 -0400

    Added alert prompt comfirm challenge

---
+++ Javascript/JS_roadtrip_pt2/userProfile.js
@@ -0,0 +1,8 @@
+var ageIsCorrect = false;
+while (ageIsCorrect == false){
+  var userAge = prompt("What's your age, user?");
+  if (confirm("You entered " + userAge + ". Is this correct?")){
+    alert("Great! Your age is logged as " + userAge + ".");
+    ageIsCorrent = true;
+  }
+}
