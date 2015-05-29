917d3b8f9d249bf0db2994841bcdb97599d6dbdf
Fred Chapman <areinei123@gmail.com>
2015-05-29 09:08:22 -0400

    added challenge for filtering

--- Javascript/JS_roadtrip_level3_pt1/JQuery_lvl3.js
+++ Javascript/JS_roadtrip_level3_pt1/JQuery_lvl3.js
@@ -49,3 +49,18 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $("button").on("click", function(){
+    var tour = $(this).closest(".tour");
+    var discount = tour.data("discount");
+    var message = $("<span>Call 1-555-jquery-air for a $" + discount + " discount.</span>");
+    tour.append(message);
+    $(this).remove();
+  });
+});
+
+$(document).ready(function(){
+  $('#filters').on('click', '.on-sale', function(){
+    
+  });
+});
