1ecb906f19b37d193db5ba12a7dc9e4cc612ebee
Fred Chapman <areinei123@gmail.com>
2015-05-30 18:25:18 -0400

    level 4 challenges

--- Javascript/JS_roadtrip_level3_pt1/JQuery_lvl4.js
+++ Javascript/JS_roadtrip_level3_pt1/JQuery_lvl4.js
@@ -16,3 +16,53 @@ $(document).ready(function() {
     $(this).find("span").slideToggle();
   });
 });
+
+$(document).ready(function() {
+  $("#tour").on("click", "button", function() {
+    $(".photos").slideToggle();
+  });
+  $(".photos").on("mouseenter", "li", function() {
+    $(this).find("span").slideToggle();
+  });
+  $(".photos").on("mouseleave", "li", function() {
+    $(this).find("span").slideToggle();
+  });
+});
+
+function showPhotos(){
+   $(this).find("span").slideToggle();
+}
+$(document).ready(function() {
+  $("#tour").on("click", "button", function() {
+    $(".photos").slideToggle();
+  });
+  $(".photos").on("mouseenter", "li", showPhotos); 
+  $(".photos").on("mouseleave", "li", showPhotos);
+});
+
+$(document).ready(function() {
+  $("#nights").on("keyup", function() {
+    $('#nights-count').text(+$(this).val());
+  });
+});
+
+$(document).ready(function() {
+  $("#nights").on("keyup", function() {
+    var nights = +$(this).val();
+    var dailyPrice = +$(this).closest(".tour").data("daily-price");
+    $("#nights-count").text($(this).val());
+    $("#total").text(nights * dailyPrice);
+  });
+});
+
+$(document).ready(function() {
+  $("#nights").on("keyup", function() {
+    var nights = +$(this).val();
+    var dailyPrice = +$(this).closest(".tour").data("daily-price");
+    $("#total").text(nights * dailyPrice);
+    $("#nights-count").text($(this).val());
+  });
+  $(this).on("focus", function() {
+    $("#nights").val(7);
+  });
+});
