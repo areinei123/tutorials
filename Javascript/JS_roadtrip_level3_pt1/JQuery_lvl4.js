92142db4bc90e29bd652c5699c200abeab8d5975
Fred Chapman <areinei123@gmail.com>
2015-05-30 17:48:53 -0400

    level 4 challenges

--- Javascript/JS_roadtrip_level3_pt1/JQuery_lvl4.js
+++ Javascript/JS_roadtrip_level3_pt1/JQuery_lvl4.js
@@ -2,3 +2,17 @@ $(document).ready(function(){
   alert($("img").length);
 });
 
+$(document).ready(function() { 
+  $("#tour").on("click", "button", function() { 
+    $(".photos").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $("#tour").on("click", "button", function() {
+    $(".photos").slideToggle();
+  });
+  $(".photos").on("mouseenter", "li", function() {
+    $(this).find("span").slideToggle();
+  });
+});
