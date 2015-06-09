8b49dc7937ffac195e7aef70366a477a239e2756
Fred Chapman <areinei123@gmail.com>
2015-06-09 15:10:34 -0400

    finished level 2

--- ember/my-app/app/app.js
+++ ember/my-app/app/app.js
@@ -9,7 +9,23 @@ var App = Ember.Application.create({
 
 App.Router.map(function(){
   this.route('about');
-  this.route('credits');
+  this.route('credits', {path: '/thanks'});
+});
+
+App.IndexController = Ember.Controller.extend({
+  productsCount: 6,
+  logo: 'images/logo-small.png',
+  time: function() {
+    return (new Date()).toDateString();
+  }.property()
+});
+
+App.AboutController = Ember.Controller.extend({
+  avatar: 'images/avatar.png',
+  contactName: 'name',
+  open: function() {
+    return ((new Date()).getDay() === 0) ? "Closed" : "Open";
+  }.property()
 });
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
