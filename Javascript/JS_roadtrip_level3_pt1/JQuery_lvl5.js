b8c124a36624edbd9c86fb570704ad983de89210
Fred Chapman <areinei123@gmail.com>
2015-06-08 23:32:31 -0400

    straggler Jquery file

--- Javascript/JS_roadtrip_level3_pt1/JQuery_lvl5.js
+++ Javascript/JS_roadtrip_level3_pt1/JQuery_lvl5.js
@@ -34,3 +34,34 @@ $(document).ready(function() {
     $(this).removeClass('highlight');
   });
 });
+
+$(document).ready(function() {
+  $(".tour").on("mouseenter", function() {
+    $(this).addClass("highlight");
+    $(this).find(".per-night").animate({"top": "-14px","opacity": "1"});
+  });
+  $(".tour").on("mouseleave", function() {
+    $(this).removeClass("highlight");
+  });
+});
+
+$(document).ready(function() {
+  $(".tour").on("mouseenter", function() {
+    $(this).addClass("highlight");
+    $(this).find(".per-night").animate({"top": "-14px","opacity": "1"}, "fast");
+  });
+  $(".tour").on("mouseleave", function() {
+    $(this).removeClass("highlight");
+  });
+});
+
+$(document).ready(function() {
+  $(".tour").on("mouseenter", function() {
+    $(this).addClass("highlight");
+    $(this).find(".per-night").animate({"top": "-14px","opacity": "1"}, "fast");
+  });
+  $(".tour").on("mouseleave", function() {
+    $(this).removeClass("highlight");
+    $(this).find(".per-night").animate({"top": "0px","opacity": "0"}, "fast");
+  });
+});
